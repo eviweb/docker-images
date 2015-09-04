938ea49d0be193d812db39605f8339ab9dca442e
Eric Villard <dev@eviweb.fr>
2015-09-04 14:35:43 +0200

    refactor tests
    extract common steps to tests/lib/func.sh
    
    Signed-off-by: Eric Villard <dev@eviweb.fr>

---
+++ tests/lib/func.sh
@@ -0,0 +1,53 @@
+#! /bin/bash
+DOCKERFILE="${DIR/tests/src}/Dockerfile"
+NAME=${DIR##*/}
+TAG="test/${NAME}:test"
+TEMPNAME="${NAME}-test-temp"
+RESULT=0
+
+function checkDockerfile()
+{
+    if [ ! -e "${DOCKERFILE}" ]; then
+        echo "File not found: ${DOCKERFILE}"
+        exit 1
+    fi
+}
+
+function buildImage()
+{
+    echo "Build the image"
+    docker build -f "${DOCKERFILE}" -t "${TAG}" --rm=true .
+}
+
+function runTests()
+{
+    local test="$1"
+    shift
+    echo "Run Tests"
+    $test $@
+    RESULT=$?
+
+    if [ "${RESULT}" == "0" ]; then
+        echo "  > Passed";
+    else
+        echo "  > Failed";
+    fi
+}
+
+function doCleanup()
+{
+    echo "Clean up"
+    docker rm "${TEMPNAME}"
+    docker rmi "${TAG}"
+}
+
+function endup()
+{
+    exit ${RESULT}
+}
+
+function checkPHPVersion()
+{
+    echo "Check for PHP version: $1"
+    docker run --name="${TEMPNAME}" "${TAG}" php --version | grep "$1" &> /dev/null
+}
--- tests/php54-cli/test.sh
+++ tests/php54-cli/test.sh
@@ -1,31 +1,13 @@
 #! /bin/bash
 DIR=$(dirname $(readlink -f "$0"))
-DOCKERFILE="${DIR/tests/src}/Dockerfile"
-NAME=${DIR##*/}
-TAG="test/${NAME}:test"
-TEMPNAME="${NAME}-test-temp"
-RESULT=0
+LIBDIR=$(dirname "${DIR}")/lib
 
-if [ ! -e "${DOCKERFILE}" ]; then
-    echo "File not found: ${DOCKERFILE}"
-    exit 1
-fi
+for f in ${LIBDIR}/*.sh; do
+    . $f
+done
 
-echo "Build the image"
-docker build -f "${DOCKERFILE}" -t "${TAG}" --rm=true .
-
-echo "Run Tests"
-docker run --name="${TEMPNAME}" "${TAG}" php --version | grep 5.4 &> /dev/null
-RESULT=$?
-
-if [ "${RESULT}" == "0" ]; then
-    echo "  > Passed";
-else
-    echo "  > Failed";
-fi
-
-echo "Clean up"
-docker rm "${TEMPNAME}"
-docker rmi "${TAG}"
-
-exit ${RESULT}
+checkDockerfile
+buildImage
+runTests 'checkPHPVersion' '5.4'
+doCleanup
+endup
